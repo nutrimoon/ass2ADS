f6a421b887568ccc104b367f450d00fc1aca9c26
nutrimoon <97510107+nutrimoon@users.noreply.github.com>
2022-04-23 15:37:17 +0600

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;  //I have only 50 from 1 ass, so here I am using java.util
+import java.util.Queue;
+public class Queue {
+
+Queue<T> example = new ArrayList<>();
+        public T peek () {
+            if (isEmpty() == true) {    //if isEmpty is true, which means that size is 0
+                System.out.println("EMPTY");
+                return null;
+            }
+            return get(example.size - 1);
+        }
+
+        public T enqueue (T newItem){
+            example.add(newItem); // add function appends new item to the end, exactly what we need for enqueue
+            return newItem;
+        }
+
+        public T dequeue () {
+            if (isEmpty() == true) {
+                System.out.println("EMPTY");
+                return null;
+            }
+            T firstElement=peek();
+
+            return example.remove(firstElement); //or we can write remove(0), but I  first retrieve then remove
+        }
+
+        public boolean isEmpty () {
+            if (example.size == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+
